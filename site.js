e47aab0ef39a8c8ef540e7a2fb14c9c8b4683de5
Adam Canady <adam@adamcanady.com>
2016-08-21 23:46:25 -0700

    Update with changes from bmr.com

--- site.js
+++ site.js
@@ -14,7 +14,7 @@
     };
   };
 
-  function loadToc($content, $toc, selector) {
+  function loadToc($content, $toc, selector, topPad) {
     var headerHeights = {};
 
     var recacheHeights = function() {
@@ -31,7 +31,7 @@
 
       var best = null;
       for (var name in headerHeights) {
-        if ((headerHeights[name] < currentTop + 20 && headerHeights[name] > headerHeights[best]) || best === null) {
+        if ((headerHeights[name] < currentTop + topPad && headerHeights[name] > headerHeights[best]) || best === null) {
           best = name;
         }
       }
@@ -42,6 +42,10 @@
     var generateToc = function() {
       var tocHtml = $content.find(selector).map(function() {
         var text = $(this).data("toc-title") || $(this).text();
+        var href;
+        if ($(this).attr("id") === undefined) {
+          $(this).attr("id", encodeURIComponent($(this).text().replace(/ /gi, "-")));
+        }
         var href = "#" + $(this).attr("id");
         return "<a class='toc-link' href='" + href + "'>" + text + "</a>";
       }).get().join('');
@@ -55,8 +59,17 @@
       refreshToc();
 
       // reload immediately after scrolling on toc click
-      $toc.find('a').click(function() {
-        setTimeout(refreshToc, 1);
+      $toc.find('a.toc-link').click(function() { // todo refactor out class names
+        setTimeout(function() {
+          refreshToc();
+          $('.toc-wrap').removeClass('visible'); // todo refactor out class names
+          $(window).scrollTop($(window).scrollTop() - topPad + 1);
+        }, 0);
+      });
+
+      $('a.toc-show-link').click(function() { // todo refactor out class names
+        $('.toc-wrap').toggleClass('visible'); // todo refactor out class names
+        return false;
       });
 
       $(window).scroll(debounce(refreshToc, 200));
@@ -66,22 +79,19 @@
     $(makeToc);
 
     window.recacheHeights = recacheHeights;
+
+    return function() {
+      var isDesktop = $(window).width() > 767;
+      $('.toc-wrap').each(function() {
+        if ($(this).offset().top <= $(document).scrollTop() + topPad && isDesktop) {
+          $(this).children('.toc').css("top", topPad + "px").addClass('floating');
+        } else {
+          $(this).children('.toc').css("top", "").removeClass('floating');
+        }
+      })
+    };
   }
 
-  var sticky = function() {
-    $('.toc-wrap').each(function() {
-      if ($(this).offset().top <= $(document).scrollTop() + 10) {
-        $(this).children('.toc').addClass('floating');
-      } else {
-        $(this).children('.toc').removeClass('floating');
-      }
-    })
-  };
 
-  $(function() {
-    loadToc($('.content'), $('.toc-content'), 'h1');
-    $(window).scroll(debounce(sticky));
-    sticky();
-  })
 
 })();
